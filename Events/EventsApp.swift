b46b9794c68b4aed4c016ade52bfc7971f4973d6
tudor popa <85358688+tudorfpopa@users.noreply.github.com>
2024-08-06 22:44:33 +0200

    Initial Commit

---
+++ Events/EventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsApp.swift
+//  Events
+//
+//  Created by Tudor Popa on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct EventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
